2921f1c6e47bc5b0e6eb74167f1d12afa018e38a
Saniya <saniya.khatik@adypu.edu.in>
2026-03-01 23:51:26 +0530

    Added Get Post By ID API

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -129,3 +129,22 @@ exports.searchPosts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+
+//   GET /api/posts/:id
+//   Get single post by ID
+
+exports.getPostById = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate("tags");
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { createPost, getAllPosts, searchPosts } = require('../controllers/post.controller');
 const upload = require('../middleware/upload.middleware');
+const { getPostById } = require('../controllers/post.controller');
+
+
 
 router.post('/', upload.single('image'), createPost);
-
-router.get('/', getAllPosts);
 router.get('/search', searchPosts);
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
 
 module.exports = router;
